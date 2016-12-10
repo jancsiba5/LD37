1eb3d62a819fcb2d0fa38a504a2fbc4a11d5c0b0
Kovács Botond János <jancsiba5@gmail.com>
2016-12-10 03:55:39 +0100

    Fourth commit

--- core/src/com/coloniergames/ld37/LudumMain.java
+++ core/src/com/coloniergames/ld37/LudumMain.java
@@ -12,22 +12,16 @@ public class LudumMain extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+            System.out.println ("Hello, Gabi!");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
