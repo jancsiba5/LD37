55b0f50dbaa54759653a616399d75a8717e22ad5
Kovács Botond János <jancsiba5@gmail.com>
2016-12-10 03:51:56 +0100

    First commit

---
+++ desktop/src/com/coloniergames/ld37/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.coloniergames.ld37.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coloniergames.ld37.LudumMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumMain(), config);
+	}
+}
